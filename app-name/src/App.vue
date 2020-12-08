463a36c1dbc615e88795df0429430bf48043c26c
renissonsilva <renissonx@gmail.com>
2020-12-08 17:42:23 -0300

    Acessando API de posts

--- app-name/src/App.vue
+++ app-name/src/App.vue
@@ -1,60 +1,44 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <v-banner elevation="12"
+        ><v-toolbar-title>Posts</v-toolbar-title></v-banner
+      >
+      <v-container class="grey lighten-5">
+        <v-row>
+          <v-col cols="4" v-for="post in posts" :key="post.id">
+            <v-card elevation="2" outlined class="ma-4">
+              <v-card-title>{{ post.title }}</v-card-title>
+              <v-card-text>{{ post.body }}</v-card-text>
+              <v-card-actions>{{ post.userId }}</v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import axios from "axios";
 
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  name: "app",
+  data() {
+    return {
+      posts: [],
+    };
   },
+  async created() {
+    try {
+      const responsePost = await axios.get(
+        `http://jsonplaceholder.typicode.com/posts`
+      );
 
-  data: () => ({
-    //
-  }),
+      this.posts = responsePost.data;
+    } catch (e) {
+      console.error(e);
+    }
+  },
 };
 </script>
